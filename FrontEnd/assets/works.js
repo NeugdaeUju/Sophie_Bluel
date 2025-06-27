bc085e5f247e9e2a94e80b9605abc484d445d141
Leïla Pelletier <leila.plltr@gmail.com>
2025-06-27 20:28:28 +0200

    Correction des noms de variables

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,21 +1,21 @@
 // Récupération des travaux depuis l'API
-const reponse = await fetch("http://localhost:5678/api/works");
-works = await reponse.json();
+const reponse = await fetch("http://localhost:5678/api/works/");
+const works = await reponse.json();
+console.log(works)
 
 // Fonction pour créer les fiches des travaux
 function generateWorks(works) {
     for(let i = 0 ; i < works.length ; i++ ) {
         const article = works[i];
-
         // Récupértion de l'élément du DOM qui accueillera les élements
-        const sectionGallery = document.querySelector(".gellery");
+        const sectionGallery = document.querySelector(".gallery");
         // Création d'une balise dédier à un travail
         const workElement = document.createElement("figure");
         // Création des balises de contenu
         const imageElement = document.createElement("img");
-        imageElement.src = works.imageUrl;
+        imageElement.src = article.imageUrl;
         const captionElement = document.createElement("figcaption");
-        captionElement.innerText = works.title;
+        captionElement.innerText = article.title;
 
         // On rattache la balise figure à la gallerie
         sectionGallery.appendChild(workElement);
