e92b5e1a78de5b60f0c091e77276903e47ed0045
Leïla Pelletier <leila.plltr@gmail.com>
2025-06-27 19:46:19 +0200

    Création de la fonction pour générer les travaux + début de la boucle de création des fiches articles

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,3 +1,11 @@
 // Récupération des travaux depuis l'API
 const reponse = await fetch("http://localhost:5678/api/works");
 works = await reponse.json();
+
+// Fonction pour créer les fiches des travaux
+function generateWorks(works) {
+    for(let i = 0 ; i < works.length ; i++ ) {
+        const article = works[i];
+    }
+
+}
